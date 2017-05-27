d64e8de45280dff2f4c6a8a437f477b741e46317
nyura123 <nyura123@gmail.com>
2017-05-27 15:47:09 -0500

    next-example merge

---
+++ next-example/next/3572355d-d4e9-4f33-9374-67496946b416/page/other/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/other', function() {
+            var comp = module.exports=webpackJsonp([3],[],[461]);
+            return { page: comp.default }
+          })
+        
